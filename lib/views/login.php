8dbc6419ad12054cbdc9638d08945b037132494e
Max <max.makkusuu@gmail.com>
2020-10-18 22:01:09 +0200

    added login page for xl devices

--- lib/views/login.php
+++ lib/views/login.php
@@ -46,136 +46,45 @@ if (isset($_POST['login'])) {
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/@tailwindcss/ui@latest/dist/tailwind-ui.min.css" rel="stylesheet">
     <link href="https://rsms.me/inter/inter.css" rel="stylesheet">
+    <style>
+        .center {
+            margin: auto;
+            width: 50%;
+            border: 3px solid green;
+            padding: 10px;
+        }
+    </style>
 
 </head>
 <body>
 
 <main>
 
-    <!-- Navbar section START d-->
-    <section>
-        <nav class="w-full bg-gray-800">
-            <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-                <div class="relative flex items-center justify-between h-16">
-                    <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                        <button aria-expanded="false"
-                                aria-label="Main menu"
-                                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out">
-                            <svg class="block h-6 w-6" fill="none" id="hamburgerbtn" stroke="currentColor"
-                                 viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M4 6h16M4 12h16M4 18h16" stroke-linecap="round" stroke-linejoin="round"
-                                      stroke-width="2"/>
-                            </svg>
-                        </button>
-                    </div>
-                    <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                        <h1 class="block md:hidden text-xl tracking-tight font-extrabold text-white sm:text-2xl sm:leading-none md:text-2xl uppercase">
-                            Max W.</h1>
-                        <h1 class="hidden md:block text-xl tracking-tight font-extrabold text-white sm:text-2xl sm:leading-none md:text-2xl uppercase">
-                            Max Weiner</h1>
-                    </div>
-                    <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                        <div class="ml-3 relative">
-                            <div class="hidden sm:block sm:ml-6">
-                                <div class="flex font-semibold">
-                                    <a class="px-3 py-2 rounded-md text-base leading-5 text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
-                                       href="#">Home</a>
-                                    <a class="ml-12 px-3 py-2 rounded-md text-base leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
-                                       href="#">Blog</a>
-                                    <button class="ml-12 px-3 py-2 font-semibold rounded-md text-base leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
-                                            id="projectBtn">Projekte
-                                    </button>
-                                    <button class="ml-12 px-3 py-2 rounded-md text-base leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
-                                            id="contactBtn">Kontaktiere
-                                        mich
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="hidden sm:hidden" id="mobileMenu">
-                <div class="px-2 pt-2 pb-3 font-semibold">
-                    <a class="block px-3 py-2 rounded-md text-base text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
-                       href="#">Home</a>
-                    <a class="mt-1 block px-3 py-2 rounded-md text-base text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
-                       href="#">Blog</a>
-                    <a class="mt-1 block px-3 py-2 rounded-md text-base text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
-                       href="#projects">Projekte</a>
-                    <a class="mt-1 block px-3 py-2 rounded-md text-base text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
-                       href="#">Kontaktiere
-                        mich</a>
-                </div>
-            </div>
-        </nav>
-    </section>
-    <!-- Navbar section END -->
 
-    <section class="p-12">
-        <div class="max-w-3xl mx-auto bg-gray-200 rounded">
-            <div class="bg-gray-800 rounded-t p-4">
-                <p class="text-white font-semibold text-base text-center">Logge dich mit deinem Account ein</p>
+    <!-- Login when screen is md or bigger -->
+    <section class="xl:block hidden h-screen" style="background: linear-gradient(to right, white 50%, #252F3F 50%);">
+        <div class="w-1/2 flex h-screen">
+            <div class="text-center p-4 absolute inset-y-0 right-0 flex">
+                <a href="https://www.google.com" class="text-gray-400 hover:text-gray-500  text-3xl mr-4"><i class="fas fa-home"></i></a>
             </div>
-            <div class="text-center p-6">
-                <h1 class="text-xl font-semibold text-center my-4">User Login</h1>
-                <form method="post">
-                    <div class="grid grid-flow-row grid-cols-1 gap-2">
-                        <div class="">
-                            <input type="text"
-                                   class="bg-gray-200 border-2 border-gray-300 w-full max-w-sm p-2 rounded-lg border-transparent focus:border-green-400"
-                                   name="username" placeholder="Benutzername">
-                        </div>
-                        <div class="">
-                            <input type="password"
-                                   class="bg-gray-200 border-2 border-gray-300 w-full max-w-sm p-2 rounded-lg border-transparent focus:border-green-400"
-                                   name="password" placeholder="Passwort">
-                        </div>
+            <div class="text-center max-w-xl m-auto">
+                <h1 class="font-semibold tracking-tight text-3xl">Logge dich ganz einfach ein <i class="fas fa-sign-in-alt"></i></h1>
+                <h3 class="mt-1 tracking-tight text-base font-medium">Gebe dein Benutzername und Passwort ein, um dich einzuloggen.</h3>
+                <div class="mt-4">
+                    <form method="post">
                         <div>
-                            <input type="submit"
-                                   class="bg-green-500 hover:bg-green-700 rounded text-white w-full cursor-pointer max-w-sm p-2"
-                                   name="login" value="Einloggen">
+                            <input type="text" class="border-2 border-gray-800 text-gray-800 placeholder-gray-800 focus:border-green-600 p-2 rounded-lg" name="username" placeholder="Benutzername">
+                            <input type="passwort" class="border-2 border-gray-800 text-gray-800 placeholder-gray-800 focus:border-green-600 p-2 rounded-lg" name="password" placeholder="Passwort">
                         </div>
-                        <div>
-                            <a href="" class="text-green-600 hover:text-green-700 hover:underline">Passwort /
-                                Benutzername vergessen?</a>
+                        <div class="mt-2">
+                            <input type="submit" class="bg-green-600 hover:bg-green-700 text-white font-semibold p-2 cursor-pointer w-1/3 rounded-lg" name="login" value="Einloggen">
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
     </section>
 
-
-    <!-- footer section START -->
-    <footer id="contactSection" class="w-full lg:fixed bottom-0">
-        <div class="bg-gray-800 p-4">
-            <div class="grid grid-flow-row grid-cols-1 gap-4 py-12">
-                <div class="mx-auto">
-                    <h1 class="text-2xl tracking-tight leading-10 font-semibold text-white sm:text-3xl sm:leading-none md:text-3xl uppercase">
-                        Meine Sozialenmedien</h1>
-                </div>
-                <div class="mx-auto">
-                    <a href="https://www.github.com/mweiner01"><i
-                                class="fab fa-github text-white hover:text-gray-400 text-4xl lg:text-5xl mr-4"></i></a>
-                    <a href="https://www.linkedin.com/in/max-weiner-8969b41b3/"><i
-                                class="fab fa-linkedin text-white hover:text-blue-700 rounded text-4xl lg:text-5xl"></i></a>
-                </div>
-                <div class="mx-auto text-white">
-                    <a href="https://www.freepik.com/vectors/design">Design vector created by freepik -
-                        www.freepik.com</a>
-                </div>
-            </div>
-            <hr>
-            <div class="grid grid-flow-row grid-cols-1 gap-4 pt-4">
-                <div class="text-white mx-auto">
-                    <h2>© 2020 Max Weiner • Alle Rechte vorbehalten </h2>
-                </div>
-            </div>
-        </div>
-    </footer>
-
-    <!-- footer section END -->
 
 </main>
 
